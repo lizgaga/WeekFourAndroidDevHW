364ded6f1822358a50c1a3652341f72e42b27dc1
Elizabeth Guo <zkg@andrew.cmu.edu>
2019-09-23 19:23:03 -0400

    Works now

--- app/src/main/java/com/example/weekfourandroiddevhw/MainActivity.java
+++ app/src/main/java/com/example/weekfourandroiddevhw/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.weekfourandroiddevhw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
 
 import android.os.Bundle;
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                createDialog("Text1.");
+            }
+        });
+
+        ListView listView = findViewById(R.id.list_view);
     }
+
+    private void createDialog(String message) {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage(message);
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        /*builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });*/
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+
 }
